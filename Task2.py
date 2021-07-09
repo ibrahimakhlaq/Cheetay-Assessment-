69fa579e940ecfe1c554097f1054b3fe3d14bdb8
ibrahimakhlaq <31385495+ibrahimakhlaq@users.noreply.github.com>
2021-07-10 03:23:21 +0500

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -3,13 +3,14 @@ from itertools import permutations
 
 
 def printLargest(arr):
-    log = []
+    max = 0
     for j in permutations(arr, len(arr)):
 
-        log.append("".join(map(str, j)))
-    return max(log)
+        temp = "".join(map(str, j))
+        if int(temp) > max:
+            max = int(temp)
 
-
+    return (max)
 arr = [3, 30, 34, 5, 9, 34, 23, 98]
 
-printLargest(arr)
+print(printLargest(arr))
